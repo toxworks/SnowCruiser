41ba8dca3976e23531b2fe09103d05c4e9d30a7c
António M. Lopes <ant.mont.art@gmail.com>
2023-07-15 06:51:25 +0200

    Player parent to moving vehicle successful
    Successful parenting of player to moving vehicle without any kind of visual stuttering.
    It works by having two different Player Controller scripts, one is the Unity default FirstPersonController.cs and this one is activated while the player is outside the vehicle, and the other is a custom player controller script (PlayerUltimateController.cs) that handles the movement while inside the vehicle (without using the character controller component), during all this there is a third script called PlayerParenting.cs that is handling the toggling on and off of both player controller scripts.
    Quite a mess, but hey IT WORKS! (for now)

--- Assets/SCRIPTS/Ultimate Player Controller/PlayerUltimateController.cs
+++ Assets/SCRIPTS/Ultimate Player Controller/PlayerUltimateController.cs
@@ -6,41 +6,21 @@ public class PlayerUltimateController : MonoBehaviour
 {
     public float speed = 5f;
     public float mouseSensitivity = 2f;
-    public float gravity = -9.81f;
     public Transform cameraTransform;
     public KeyCode forwardKey = KeyCode.W;
     public KeyCode backwardKey = KeyCode.S;
     public KeyCode leftKey = KeyCode.A;
     public KeyCode rightKey = KeyCode.D;
 
-    private CharacterController characterController;
-    private Vector3 velocity;
     private float xRotation = 0f;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        /*
-        // old player movement - glitches player movement when carController.cs is pointed towards global X axis
-        float x = 0f;
-        float z = 0f;
-        if (Input.GetKey(forwardKey))
-            z += 1f;
-        if (Input.GetKey(backwardKey))
-            z -= 1f;
-        if (Input.GetKey(leftKey))
-            x -= 1f;
-        if (Input.GetKey(rightKey))
-            x += 1f;
-        Vector3 move = transform.right * x + transform.forward * z;
-        characterController.Move(move * speed * Time.deltaTime);
-        */
-
         // player movement
         Vector3 moveDirection = Vector3.zero;
         if (Input.GetKey(forwardKey))
@@ -61,9 +41,5 @@ public class PlayerUltimateController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -85f, 85f);
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
-
-        // player gravity
-        velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
     }
 }
